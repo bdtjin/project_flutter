9cb2fcfad62f45d940f2df77466d4c4ce6addc0a
jin <bdtjin00@gmail.com>
2025-12-15 14:52:07 +0900

    feat: 스토어 앱 만들기

--- flutter_store_app/lib/main.dart
+++ flutter_store_app/lib/main.dart
@@ -9,13 +9,46 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: StorePage());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: StorePage());
   }
 }
 
 class StorePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            // text or image -> 추가할 내용 작성
+            SizedBox(
+              height: 70,
+              child: Padding(
+                padding: EdgeInsetsGeometry.symmetric(horizontal: 20),
+                child: Row(
+                  children: [
+                    Text(
+                      'Women',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Spacer(),
+                    Text('Kids', style: TextStyle(fontWeight: FontWeight.bold)),
+                    Spacer(),
+                    Text(
+                      'Shose',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Spacer(),
+                    Text('Bag', style: TextStyle(fontWeight: FontWeight.bold)),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(child: Image.asset('assets/bag.jpg', fit: BoxFit.cover)),
+            Expanded(child: Image.asset('assets/shoes.jpg', fit: BoxFit.cover)),
+          ],
+        ),
+      ),
+    );
   }
 }
